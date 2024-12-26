5b4596e7523559a225a82a2dd26145dd39ca34bb
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-12-26 14:40:40 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -1,1 +1,58 @@
+import asyncio, logging, os, sys
+
+from os import getenv
+from dotenv import load_dotenv
+from typing import Union, List, Pattern
+from pyrogram import Client, filters, idle
+from logging.handlers import RotatingFileHandler
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+logging.basicConfig(
+    format="[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+logs = logging.getLogger()
+loop = asyncio.get_event_loop()
+
+
+if os.path.exists("Config.env"):
+    load_dotenv("Config.env")
+
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", 0))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+START_IMAGE_URL = getenv("START_IMAGE_URL", None)
+
+
+def cdx(commands: Union[str, List[str]]):
+    return filters.command(commands, ["/", "!", "."])
+
+def cdz(commands: Union[str, List[str]]):
+    return filters.command(commands, ["", "/", "!", "."])
+
+def rgx(pattern: Union[str, Pattern]):
+    return filters.regex(pattern)
+
+
+bot = Client(
+    name="Bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+mongodb = AsyncIOMotorClient(MONGO_DB_URL).test
 
