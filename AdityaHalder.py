b4da752df30def02f3a1dadfbe6cef35181b2b78
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-12-27 16:04:09 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -1,4 +1,4 @@
-import asyncio, logging, os, sys
+import asyncio, logging, openapi, os, sys
 
 from os import getenv
 from dotenv import load_dotenv
@@ -39,6 +39,7 @@ API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+OPEN_API_KEY = getenv("OPEN_API_KEY", None)
 OWNER_ID = int(getenv("OWNER_ID", 0))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 START_IMAGE_URL = getenv("START_IMAGE_URL", None)
@@ -76,6 +77,9 @@ async def main():
         sys.exit()
     if not MONGO_DB_URL:
         logs.info("'MONGO_DB_URL' - Not Found !!")
+        # sys.exit()
+    if not OPEN_API_KEY:
+        logs.info("'OPEN_API_KEY' - Not Found !!")
         # sys.exit()
     if OWNER_ID == 0:
         logs.info("'OWNER_ID' - Not Found !!")
